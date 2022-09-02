f621f84f19dc3dad17b6c85d3045793d20a21982
Shiryaeva Galina <Galla1777.35@gmail.com>
2022-09-02 19:04:03 +0300

    Set listeners on bottom navigation items

--- app/src/main/java/ru/app/healer/activity/AppActivity.kt
+++ app/src/main/java/ru/app/healer/activity/AppActivity.kt
@@ -2,12 +2,16 @@ package ru.app.healer.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.commit
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import ru.app.healer.CalendarFragment
+import ru.app.healer.ProfileFragment
 import ru.app.healer.R
 import ru.app.healer.databinding.ActivityMainBinding
+import ru.app.healer.fragment.HomeFragment
 
 class AppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,43 +20,54 @@ class AppActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navController = findNavController(R.id.nav_host_fragment)
-        val appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.home,
-            R.id.calendar,
-            R.id.profile
-        ))
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.home,
+                R.id.calendar,
+                R.id.profile
+            )
+        )
         setupActionBarWithNavController(navController, appBarConfiguration)
         binding.bottomNavigation.setupWithNavController(navController)
 
-        //TODO("отладить переключение фрагментов по кликам в меню навигации (пример: https://www.youtube.com/watch?v=wzu380RouwI)")
+        //TO DO("отладить переключение фрагментов по кликам в меню навигации (пример: https://www.youtube.com/watch?v=wzu380RouwI)")
 
         binding.bottomNavigation.setOnItemSelectedListener { item ->
-            when(item.itemId) {
+            when (item.itemId) {
                 R.id.home -> {
-                    //
+                    supportFragmentManager.commit {
+                        replace(R.id.container, HomeFragment())
+                        addToBackStack(HomeFragment::class.simpleName)
+                    }
                     true
                 }
                 R.id.calendar -> {
-                    //
+                    supportFragmentManager.commit {
+                        replace(R.id.container, CalendarFragment())
+                        addToBackStack(CalendarFragment::class.simpleName)
+                    }
                     true
                 }
                 R.id.profile -> {
-                    //
+                    supportFragmentManager.commit {
+                        replace(R.id.container, ProfileFragment())
+                        addToBackStack(ProfileFragment::class.simpleName)
+                    }
                     true
                 }
                 else -> false
             }
         }
 
-        binding.bottomNavigation.setOnItemReselectedListener { item ->
-            when(item.itemId) {
-                R.id.home -> {
-                    //
-                    true
-                }
-                else -> false
-            }
-        }
+//        binding.bottomNavigation.setOnItemReselectedListener { item ->
+//            when (item.itemId) {
+//                R.id.home -> {
+//                    //
+//                    true
+//                }
+//                else -> false
+//            }
+//        }
     }
 }
 
